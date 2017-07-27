feef74ee7497753759f7475d398611ee325746e9
skaltamas <skaltamas123@gmail.com>
2017-07-27 12:05:08 +0530

    Third commit:evaluation

---
+++ postfix.c
@@ -0,0 +1,88 @@
+#include<stdio.h>
+#include<ctype.h>
+#define MAX_SIZE 30
+
+typedef struct conversion
+{
+	char a[30];
+	int top;
+}stack;
+
+int push(stack *,char);
+char pop(stack*);
+int operation(int,int,char);
+int evaluate(char []);
+
+int main()
+{
+	char postfix[30];
+	printf("Enter POSTFIX Expression\n");
+	scanf("%s",postfix);
+	printf("Evaluation of postfix expresssion =%d",evaluate(postfix));
+	return 0;
+}
+
+int push(stack *s,char opr)
+{
+	if (s->top==MAX_SIZE-1)
+	return 1;
+	else
+	{
+	s->top++;
+	s->a[s->top]=opr;
+	return 0;
+	}
+}
+
+char pop(stack *s)
+{
+	if (s->top==-1)
+	{
+		printf("stack is empty\n");
+		return 0;
+    }
+    else
+    {
+		char data=s->a[s->top--];
+		return data;
+    }
+}
+
+int operation(int num1,int num2,char opr)
+{
+	switch(opr)
+	{
+		case '+':return num1+num2;
+		break;
+		case '-':return num1-num2;
+		break;
+		case '*':return num1*num2;
+		break;
+		case '/':return num1/num2;
+		break;
+	}
+}
+
+int evaluate(char pos[])
+{
+	int result,e1,e2,i;
+	char opr;
+	stack s;
+	s.top=-1;
+	for(i=0;pos[i]!='\0';i++)
+	{
+		opr=pos[i];
+		if(isdigit(opr))
+		push(&s,opr-'0');
+		else
+		{
+		e1=pop(&s);
+		e2=pop(&s);
+		result=operation(e1,e2,opr);
+		push(&s,result);
+		}
+	}
+	return pop(&s);
+}
+
+
